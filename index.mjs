3772602a3928db8dbe0811fe51941acf605e3e9d
Dickens Lian <dickenslian@tencent.com>
2019-07-22 12:42:05 +0800

    replace require with import

--- index.mjs
+++ index.mjs
@@ -6,6 +6,14 @@ import generator from "@babel/generator";
 import t from "@babel/types";
 import traverser from "@babel/traverse";
 
+function generateImp(name, module) {
+  const id = t.identifier(name);
+  const sp = t.importDefaultSpecifier(id);
+  const literal = t.stringLiteral(module);
+
+  return t.importDeclaration([sp], literal);
+}
+
 const generate = generator.default;
 const traverse = traverser.default;
 
@@ -13,24 +21,36 @@ const code = `
 define("modules/bdm/index/index", function(require, exports, module) {
   var $ = require("$");
   var pageManager = require("pageManager");
-
-});
-`;
+  
+  
+})`;
 const ast = parser.parse(code);
 
-const content = ast.program.body[0].expression.arguments[1].body;
-
+const content = ast.program.body[0].expression.arguments[1].body.body;
 traverse(ast, {
   ExpressionStatement(path) {
-    
-    if (path.parent.type === 'Program') {
-      path.insertAfter(content)
-      // 删除原methods
-      path.remove()
+    if (path.parent.type === "Program") {
+      // 插入正文内容
+      content.forEach(node => {
+        path.insertBefore(node);
+      });
+
+      // 删除 define
+      path.remove();
+    }
+  },
+  VariableDeclaration(path) {
+    const init = path.node.declarations[0].init;
+    if (init.type === "CallExpression" && init.callee.name === "require") {
+      const module = init.arguments[0].value;
+      const name = path.node.declarations[0].id.name;
+      const imp = generateImp(name, module);
+
+      path.insertAfter(imp);
+      path.remove();
     }
   }
-})
-
+});
 
 const output = generate(ast, {}, code);
 
